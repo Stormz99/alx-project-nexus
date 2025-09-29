394c9758497b7cd5aa0f8886ff67512d1d7dface
Abiodun <ijiolastormz@gmail.com>
2025-09-29 01:48:48 +0100

    Fix Django settings import path

--- quickcart_backend/api_backend/asgi.py
+++ quickcart_backend/api_backend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickcart_backend.api_backend.settings')
 
 application = get_asgi_application()
